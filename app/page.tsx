ebb2093bf9ed1b0652bb6250834b64ff06a1ea5d
shotake0222 <85877535+shotake0222@users.noreply.github.com>
2026-04-07 17:33:03 +0900

    修正

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <div style={{ padding: '40px', textAlign: 'center' }}>
+        <h1>物件専用ポータル</h1>
+        <p>URLの末尾に「/rooms/1」などを入力してアクセスしてください。</p>
+      </div>
+    </main>
+  );
+}
