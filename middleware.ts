3098db7be9f27cef6d8b12b01d3a7b374e5256f0
shotake0222 <85877535+shotake0222@users.noreply.github.com>
2026-04-23 21:25:06 +0900

    修正

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,15 @@
-// middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  // ブラウザが favicon.ico を探そうとしたら、奥に通さずここで追い返す
   if (request.nextUrl.pathname.includes('favicon.ico')) {
     return new NextResponse(null, { status: 200 });
   }
+
   return NextResponse.next();
 }
 
-export const config = { matcher: '/:path*' };
+export const config = {
+  matcher: '/:path*',
+};
