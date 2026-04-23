6eb22c5bce3fd3f6918649981d3e739fd01fa2c2
shotake0222 <85877535+shotake0222@users.noreply.github.com>
2026-04-23 11:19:52 +0900

    修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        {/* これを追記：ビルドエラーを無視してブラウザで強制的にTailwindを当てる */}
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
       <body className="antialiased">{children}</body>
     </html>
   );
