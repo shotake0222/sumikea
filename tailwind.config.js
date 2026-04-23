b97217c0dce5f0e8f980ab6a744914901ce36618
shotake0222 <85877535+shotake0222@users.noreply.github.com>
2026-04-23 10:38:39 +0900

    修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",      // ここ！
-    "./components/**/*.{js,ts,jsx,tsx,mdx}", // ここ！
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",        // appフォルダ内の全ファイル
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // appの外にあるcomponentsフォルダ
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",        // supabase等が入っているlibフォルダ
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        posutto: '#2563eb', // ぽすっとブルー
+      },
+    },
   },
   plugins: [],
 }
