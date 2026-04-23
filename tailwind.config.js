1072c0976c5bd822982fe4a504df0933a7dc4de1
shotake0222 <85877535+shotake0222@users.noreply.github.com>
2026-04-23 10:56:57 +0900

    修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",      // appフォルダの中すべて
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",    // もしpagesフォルダがあれば
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // componentsフォルダ
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",      // もしsrcフォルダがあれば
   ],
   theme: {
     extend: {},
